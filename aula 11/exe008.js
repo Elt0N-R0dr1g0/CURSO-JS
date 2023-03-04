45e194b1062da005ba5e2db76473326031322a74
Elt0N-R0dr1g0 <110944392+Elt0N-R0dr1g0@users.noreply.github.com>
2023-03-04 20:12:26 -0300

    first commit

---
+++ aula 11/exe008.js
@@ -0,0 +1,7 @@
+var vel = 150.5
+console.log(`A velocidade do seu carro é ${vel} KM/h`)
+if (vel >60) { // Condição simples
+    console.log('Você ultrapassou a velocidade permitida, Multado!')   
+}
+    
+console.log('Diriga sempre usando sinto de segurança')
